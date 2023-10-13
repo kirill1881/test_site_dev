b3cc44de857071b106c89128ceb836b12a0b1c09
ddmtriev <markousthecat@gmail.com>
2023-10-14 01:14:18 +0300

    Now it works + submit form

--- test_web/test_web/test_main/urls.py
+++ test_web/test_web/test_main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
     # path('', views.index),
     # path('test/', views.test_view),
     path('', SubjectView.as_view(), name='subject_view'),
-    path('test/<slug:test_slug>/', TestView.as_view(), name='test_view'),
-    path('questions/<int:test_id>', QuestionsView.as_view(), name='questions_list')
+    path('tests/subject/<int:subject_id>/', TestView.as_view(), name='test_view'),
+    path('questions/<int:test_id>', QuestionsView.as_view(), name='questions_list'),
+    path('send_form/', views.submit_answer, name='submit_answer')
 ]
--- test_web/test_web/test_main/views.py
+++ test_web/test_web/test_main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
@@ -14,21 +14,29 @@ def test_view(request):
     return HttpResponse('<h1>Страница с тестами</h1>')
 
 
+def submit_answer(request):
+    if request.method == 'POST':
+        for question in Question.objects.all():
+            answer = request.POST.get(str(question.id))
+            print(answer)
+    return render(request, 'message.html')
+
+
 class SubjectView(ListView):
     model = Subject
     template_name = 'test_main/index.html'
     context_object_name = 'subject_list'
 
 
-class TestView(DetailView):
+class TestView(ListView):
     model = Test
     template_name = 'test_main/test_view.html'
-    context_object_name = 'test'
-    slug_url_kwarg = 'test_slug'
+    context_object_name = 'tests'
+    # slug_url_kwarg = 'test_slug'
 
     def get_queryset(self):
-        test_slug = self.kwargs['test_slug']
-        return Test.objects.filter(slug=test_slug)
+        subject = get_object_or_404(Subject, id=self.kwargs['subject_id'])
+        return Test.objects.filter(test_subject=subject)
 
 
 class QuestionsView(ListView):
@@ -37,5 +45,5 @@ class QuestionsView(ListView):
     context_object_name = 'questions'
 
     def get_queryset(self):
-        # question_id = self.kwargs['pk']
-        return Question.objects.all().select_related('test_title')
+        test_id = get_object_or_404(Test, id=self.kwargs['test_id'])
+        return Question.objects.filter(test_title=test_id)
